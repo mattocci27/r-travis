16a6a8d74c8caef3a3783774d3b8c27b7f1499b7
Masatoshi Katabuchi <mattocci27@gmail.com>
2020-07-27 18:23:57 +0800

    removed r-recommende

--- run.sh
+++ run.sh
@@ -139,7 +139,7 @@ BootstrapLinux() {
     # --as-cran checks:
     #   https://stat.ethz.ch/pipermail/r-help//2012-September/335676.html
     # May 2020: we also need devscripts for checkbashism
-    Retry sudo apt-get install -y --no-install-recommends r-base-dev r-recommended qpdf devscripts
+    Retry sudo apt-get install -y --no-install-recommends r-base-dev qpdf devscripts
 
     # Change permissions for /usr/local/lib/R/site-library
     # This should really be via 'staff adduser travis staff'
